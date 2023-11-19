a0bfa79b13804b0d1b5fadd8b53eda188390e911
Haddar Beiser <haddaro@gmail.com>
2023-11-19 08:10:32 +0200

    creatReviewcomments

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,10 +5,14 @@ const Review = require('../models/reviewModel');
 const factory = require('./handlerFactory');
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Business rule: Users can only review if they have completed an order with the subject.
+  // This check ensures reviews are based on actual transactions.
   const author = await User.findById(req.user._id);
   const subject = await User.findById(req.params.id);
   if (!author || !subject)
     return next(new AppError('Cannot submit a review', 400));
+
+  // Prevent duplicate reviews: a user should not review the same subject more than once.
   if (!(await author.completedOrderTo(subject._id)))
     return next(
       new AppError('Please complete an order an then write a review', 400),
@@ -17,9 +21,16 @@ exports.createReview = catchAsync(async (req, res, next) => {
     return next(
       new AppError(`You already wrote a review on ${subject.name}`, 400),
     );
+
+  // Assign the author and subject to the review being created.
+  // This links the review clearly to both the reviewer and the subject of the review.
   req.body.author = author._id;
   req.body.subject = subject._id;
+
+  // Create and store the new review in the database.
   const review = await Review.create(req.body);
+
+  // Respond with the newly created review.
   res.status(201).json({ status: 'success', data: { review } });
 });
 
